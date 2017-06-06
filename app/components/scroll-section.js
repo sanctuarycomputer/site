40c6a9961a5450c66675aa928c76ed565f1b32cd
Jay Goss <jaygoss@gmail.com>
2017-06-06 16:47:45 -0400

    use ember in view

--- app/components/scroll-section.js
+++ app/components/scroll-section.js
@@ -1,8 +1,6 @@
 import InViewportMixin from 'ember-in-viewport';
 import Ember from 'ember';
-// import v from 'npm:vudu';
-// import c, { vars } from 'site/lib/vudu';
-//
+
 const {
   Component,
   inject: { service },
@@ -10,32 +8,13 @@ const {
 
 export default Component.extend(InViewportMixin, {
   sanctu: service(),
-  active: false,
   viewportOptionsOverride: Ember.on('didInsertElement', function () {
-    console.log('start')
     Ember.setProperties(this, {
       viewportSpy: true,
     });
   }),
 
   didEnterViewport() {
-    console.log('hit');
-    this.set('active', true);
-    // this.sendAction('action');
-  },
-
-  didExitViewport() {
-    console.log('exit');
-    this.set('active', false);
+    this.sendAction('action');
   },
 });
-//
-// export default Ember.Component.extend(InViewportMixin, {
-//   didEnterViewport() {
-//     console.log('entered');
-//   },
-//
-//   didExitViewport() {
-//     console.log('exited');
-//   }
-// });
