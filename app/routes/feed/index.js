c71c1f5d251214d860f9a6aa9fdef611cc757551
Sebastian Odell <sebastian@sanctuary.computer>
2017-06-08 11:30:59 -0400

    filters feedData on show route

--- app/routes/feed/index.js
+++ app/routes/feed/index.js
@@ -10,25 +10,16 @@ const {
 } = Ember;
 
 const styles = v({
-  feedRoute: {
-    top: 0
-  },
-  feedWrapper: {
-    '@composes': [c.px5]
-  },
+  feedItem: {
+    borderBottom: `1px solid ${c.black.color}`,
+    display: 'flex'
+  }
 });
 
 export default Route.extend({
-  ajax: service(),
   sanctu: service(),
 
   setupController(controller) {
-    if (!get(this, 'sanctu.feedData')) {
-      get(this, 'ajax').request('https://sanctucompu-medium.herokuapp.com/').then(data => {
-        set(this, 'sanctu.feedData', data.rss.channel.item);
-      });
-    }
-
     set(controller, 'sanctu', get(this, 'sanctu'));
     set(controller, 'styles', styles);
     set(controller, 'v', v(c));
