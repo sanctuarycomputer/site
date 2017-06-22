9297f05cd9e13e8c22f4629ed11df7980e012227
Jay Goss <jaygoss@gmail.com>
2017-06-22 13:13:57 -0400

    clouds handle resize

--- app/components/cloud-call.js
+++ app/components/cloud-call.js
@@ -13,7 +13,7 @@ const styles = v({
     left: 0,
     zIndex: 0,
     'canvas': {
-      backgroundColor: '#073763'
+      backgroundColor: '#173963'
     }
   }
 });
@@ -22,32 +22,53 @@ export default Component.extend({
   classNames: [styles.cloudCall],
   styles,
   didInsertElement() {
-    const mobile = $(window).width() < 500;
+    let w = $(window).width();
+    let h =  $(window).height();
+    let mobile = w < 500;
     let renderer = null;
     let scene = null;
     let camera = null;
+    let smokeTexture = null;
     let clock = null;
     let delta = null;
     let smokeParticles = null;
     let container = this.element;
 
+    const resize = () => {
+      if (!camera || !renderer) return;
+      w = $(window).width();
+      h =  $(window).height();
+      camera.aspect = w / h;
+      camera.updateProjectionMatrix();
+      renderer.setSize(w, h);
+    }
+
     const init = () => {
+      window.addEventListener('resize', resize, false);
       clock = new THREE.Clock();
       renderer = new THREE.WebGLRenderer({ alpha: true });
-      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setSize(w, h);
       scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+      camera = new THREE.PerspectiveCamera(75, w / h, 1, 10000);
       camera.position.z = 1000;
       scene.add(camera);
 
-      const light = new THREE.DirectionalLight(0xffffff, 0.5);
+      const light = new THREE.DirectionalLight(0xfff, 0.5);
       light.position.set(-1, 0, 1);
       scene.add(light);
 
-      const smokeTexture = THREE.ImageUtils.loadTexture('/images/smoke.png');
+      const loader = new THREE.TextureLoader();
+      loader.load(
+        '/images/smoke.png',
+        (texture) => {
+          smokeTexture = texture;
+          generateSmoke();
+        }
+      )
+    };
 
-      // TODO: Adjust Emissive & Color
-      const smokeMaterial = new THREE.MeshPhongMaterial({
+    const generateSmoke = () => {
+      const smokeMaterial = new THREE.MeshStandardMaterial({
         color: 0x000,
         emissive: 0x000,
         map: smokeTexture,
@@ -56,7 +77,7 @@ export default Component.extend({
 
       const smokeGeo = new THREE.PlaneGeometry(300, 300);
       smokeParticles = [];
-      for (let p = 0; p < 45; p++) {
+      for (let p = 0; p < 45; p += 1) {
         const particle = new THREE.Mesh(smokeGeo, smokeMaterial);
         particle.position.set(
           Math.random() * 500 - 250,
@@ -68,7 +89,7 @@ export default Component.extend({
         smokeParticles.push(particle);
       }
       container.appendChild(renderer.domElement);
-    };
+    }
 
     const evolveSmoke = () => {
       if (!smokeParticles) return;
@@ -76,9 +97,7 @@ export default Component.extend({
       while (sp -= 1) smokeParticles[sp].rotation.z += delta * 0.06;
     }
 
-    const render = () => {
-      renderer.render(scene, camera);
-    }
+    const render = () => renderer.render(scene, camera);
 
     const animate = () => {
       delta = clock.getDelta();
