3729aeb225b0b8849b800366fa6f43a0efcb4fc4
Sebastian Odell <sebastian@sanctuary.computer>
2017-06-26 11:31:17 -0400

    fixes nav space

---
+++ app/transitions/fade-when-ready.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import c, { vars } from 'site/lib/vudu';
+import {
+  isAnimating,
+  finish,
+  timeSpent,
+  animate,
+  stop,
+  Promise,
+} from "liquid-fire";
+
+export default function fadeWhenReady(opts={}) {
+  debugger;
+
+
+};
