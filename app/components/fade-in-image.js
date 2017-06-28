b7290621a808806ebb9dff70c8eec75e0a05cfa7
Sebastian Odell <sebastian@sanctuary.computer>
2017-06-28 18:28:48 -0400

    adds image loading on scroll

--- app/components/fade-in-image.js
+++ app/components/fade-in-image.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import InViewportMixin from 'ember-in-viewport';
 import v from 'npm:vudu';
 import c, { breakpoints } from 'site/lib/vudu';
 
@@ -14,17 +15,23 @@ const styles = v({
     transition: '500ms ease-in-out',
     opacity: 0,
   },
-  isLoaded: {
+  fadeIn: {
     opacity: 1,
   },
 });
 
-export default Component.extend({
-  classNameBindings: [`isLoaded:${styles.isLoaded}`],
+export default Component.extend(InViewportMixin, {
+  classNameBindings: [`fadeIn:${styles.fadeIn}`],
   classNames: [styles.fadeInImage],
   imageSrc: null,
+  fadeIn: Ember.computed.and('isInView', 'isLoaded'),
+  isInView: false,
   isLoaded: false,
   styles,
+
+  didEnterViewport() {
+    set(this, 'isInView', true);
+  },
 
   didInsertElement() {
     this._super(...arguments);
